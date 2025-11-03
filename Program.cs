709d5bcc0c11760af572f4746b5fc92eed4773cd
Miguel-Abbate <abbatejr@gmail.com>
2025-11-03 10:57:32 -0300

    solução

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
-﻿double peso;
+﻿using System.Drawing;
+
+double peso;
 double altura;
 double imc;
 
 string diagnostico;
-
-Console.WriteLine("-- Calculadora de IMC --\n");
-
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("                                                         -- Calculadora de IMC --\n");
+Console.ResetColor();
 Console.Write("Digite seu peso em kg...: ");
 peso = Convert.ToDouble(Console.ReadLine());
 
@@ -13,49 +16,44 @@ Console.Write("Digite sua altura em m..: ");
 altura = Convert.ToDouble(Console.ReadLine());
 
 imc = peso / Math.Pow(altura, 2);
-//ou
-//imc = peso / (altura * altura);
-
-/*
-        17      18,5        25      30      35      40
---------|---------|----------|-------|------|--------|---------
--------- (imc < 17)
-        ---------- (imc < 18.5)
-                  ----------- (imc < 25)
-                             -------- (imc < 30)
-                                      ------ (imc < 35)
-                                              ------- (imc < 40)
-                                                                 ----------------------------- else
-*/
 
 if (imc < 17)
 {
+    Console.ForegroundColor = ConsoleColor.Blue;
     diagnostico = "Muito abaixo do peso";
 }
 else if (imc < 18.5)
 {
+    Console.ForegroundColor = ConsoleColor.Yellow;
     diagnostico = "Abaixo do peso";
 }
 else if (imc < 25)
 {
+    Console.ForegroundColor = ConsoleColor.Green;
     diagnostico = "Peso normal";
 }
 else if (imc < 30)
 {
+    Console.ForegroundColor = ConsoleColor.Yellow;
     diagnostico = "Acima do peso";
+    Console.ResetColor();
 }
 else if (imc < 35)
 {
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
     diagnostico = "Obesidade I";
 }
 else if (imc < 40)
 {
+    Console.ForegroundColor = ConsoleColor.Red;
     diagnostico = "	Obesidade II (severa)";
 }
 else
 {
-    diagnostico = "	Obesidade III (mórbida)";
+    Console.BackgroundColor = ConsoleColor.Red;
+    diagnostico = "Obesidade III (mórbida)";
 }
 
 Console.WriteLine($"\nSeu IMC é {imc:N2} kg/m².");
-Console.WriteLine($"Diagnóstico: {diagnostico}");
+Console.Write($"Diagnóstico: {diagnostico}");
+Console.ResetColor();
